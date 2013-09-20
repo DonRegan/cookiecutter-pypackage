480f30c879aa3ee4775f7e818739332e9eab9c4f
Joel Akeret <jakeret@phys.ethz.ch>
2013-09-20 10:12:22 +0200

    initial check in

---
+++ cookiecutter-pypackage/{{cookiecutter.project_name}}/docs/api.rst
@@ -0,0 +1,8 @@
+.. _api:
+
+API
+***
+
+.. automodule:: {{ cookiecutter.repo_name }}
+
+This page details the methods and classes provided by the ``{{ cookiecutter.project_name }}`` module.
---
+++ cookiecutter-pypackage/{{cookiecutter.project_name}}/setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from setuptools.command.test import test as TestCommand
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+readme = open('README.rst').read()
+doclink = """
+Documentation
+-------------
+
+The full documentation can be generated with Sphinx"""
+
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+requires = []
+
+setup(
+    name='{{ cookiecutter.repo_name }}',
+    version='{{ cookiecutter.version }}',
+    description='{{ cookiecutter.project_short_description }}',
+    long_description=readme + '\n\n' + doclink + '\n\n' + history,
+    author='{{ cookiecutter.full_name }}',
+    author_email='{{ cookiecutter.email }}',
+    url='{{ cookiecutter.project_url }}',
+    packages=[
+        '{{ cookiecutter.repo_name }}',
+    ],
+    package_dir={'{{ cookiecutter.repo_name }}': '{{ cookiecutter.repo_name }}'},
+    include_package_data=True,
+    install_requires=requires,
+    license='MIT',
+    zip_safe=False,
+    keywords='{{ cookiecutter.repo_name }}',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        "Intended Audience :: Science/Research",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+    tests_require=['pytest>=2.3.5'],
+    cmdclass = {'test': PyTest},
+)
