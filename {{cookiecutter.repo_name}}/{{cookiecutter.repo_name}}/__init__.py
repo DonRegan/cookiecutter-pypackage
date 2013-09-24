a34a3e3725233dbdaf0ec4a526bbe7bc7aeaa8d9
Joel Akeret <jakeret@phys.ethz.ch>
2013-09-24 14:05:03 +0200

    fixed typo

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
@@ -1,4 +1,4 @@
 __author__ = '{{ cookiecutter.full_name }}'
 __email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
-__credits__   = '{{ cookiecutter.credit }}'
+__credits__   = '{{ cookiecutter.credits }}'
