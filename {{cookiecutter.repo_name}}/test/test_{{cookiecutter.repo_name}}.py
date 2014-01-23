e223b1713642776bb767cb731ef207a9f3d3e3c9
Joel Akeret <jakeret@phys.ethz.ch>
2014-01-23 16:56:14 +0100

    added test example

--- {{cookiecutter.repo_name}}/test/test_{{cookiecutter.repo_name}}.py
+++ {{cookiecutter.repo_name}}/test/test_{{cookiecutter.repo_name}}.py
@@ -4,6 +4,8 @@
 """
 Tests for `{{ cookiecutter.repo_name }}` module.
 """
+from __future__ import print_function, division, absolute_import, unicode_literals
+
 import pytest
 from {{ cookiecutter.repo_name }} import {{ cookiecutter.repo_name }}
 
@@ -11,6 +13,8 @@ from {{ cookiecutter.repo_name }} import {{ cookiecutter.repo_name }}
 class Test{{ cookiecutter.repo_name|capitalize }}(object):
 
     def setup(self):
+        #prepare unit test. Load data etc
+        print("setting up " + __name__)
         pass
 
     def test_something(self):
@@ -18,4 +22,6 @@ class Test{{ cookiecutter.repo_name|capitalize }}(object):
         assert x==1
 
     def teardown(self):
+        #tidy up
+        print("tearing down " + __name__)
         pass
